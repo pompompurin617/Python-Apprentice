58bbc4dc5133229ee6f04ff002cb0641a9e12f99
pompompurin617 <swilliams@stmesc.org>
2024-10-30 20:26:57 +0000

    I LOVE POMPOMPURIN!!!!

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -10,14 +10,20 @@ to see how to use the turtle commands.
 
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+turtle.setup(width=600, height=600, startx=0, starty=0)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
-
+tina.left(90)
 # Use tina.forward(20) and tina.left(90) to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
+tina.forward(40)
+tina.left(90)
+tina.forward(40)
+tina.left(90)
+tina.forward()
 
-... # Your code here
 
-turtle.exitonclick(20)                    # Close the window when we click on it
+... # Your code here tina. forward (90)
+
+turtle.exitonclick()                    # Close the window when we click on it
